c5d63e99156518ab08889f3554babb82aba03aec
salma549 <salmasanubar786@gmail.com>
2024-10-13 11:47:19 +0530

    Weather App Using React

---
+++ weather-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import WeatherCom from './components/Weather'
+
+const App = () => {
+  return (
+    <div>
+      <WeatherCom/>
+    </div>
+  )
+}
+
+export default App
